c5ebb998e6d69728c627cf261256dc862d1eeffd
glrmrissi <guilhermerissi04112006@gmail.com>
2025-08-20 11:13:07 -0300

    fix: role in DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,5 @@
-import { IsString, IsEmail, IsStrongPassword, IsOptional, IsDateString } from "class-validator";
+import { IsString, IsEmail, IsStrongPassword, IsOptional, IsDateString, IsNumber, IsEnum } from "class-validator";
+import { Role } from "src/enums/role.enum";
 
 export class CreateUserDTO {
 
@@ -20,5 +21,9 @@ export class CreateUserDTO {
     @IsOptional()
     @IsDateString()
     birthAt: string;
+    
+    @IsOptional()
+    @IsEnum(Role)
+    role: number
 }
 
