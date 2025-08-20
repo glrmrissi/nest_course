96e084bfbf0593413776aeb6cd2ea6d946322e54
glrmrissi <guilhermerissi04112006@gmail.com>
2025-08-20 11:12:00 -0300

    feat: using guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,41 +1,51 @@
-import { Body, Controller, Delete, Get, Patch, Post, Put, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 import { UserService } from './user.service';
 import { LogInterceptor } from 'src/interceptors/log.interceptor';
 import { ParamId } from 'src/decorators/param-id.decorator';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
+import { RoleGuard } from 'src/guards/role.guard';
+import { AuthGuard } from 'src/guards/auth.guard';
 
+@UseGuards(AuthGuard, RoleGuard)
 @UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
+    @Roles(Role.Admin)
     @Post()
     async create(@Body() data: CreateUserDTO){
         return this.userService.create(data);
     }
-
+    
+    @Roles(Role.Admin)
     @Get()
     async show(){
         return this.userService.show();
     }
-
+    
     @Get(':id')
     async showOne(@ParamId() id: number){
         return this.userService.showOne(id);
     }
-
+    
+    @Roles(Role.Admin)
     @Put(':id')
     async update(@Body() data: UpdatePutUserDTO, @ParamId() id: number) {
         this.userService.update(id , data)
     }
-
+    
+    @Roles(Role.Admin)
     @Patch(':id')
     async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id: number) {
         this.userService.updatePartial(id , data)
     }
-
+    
+    @Roles(Role.Admin)
     @Delete(':id')
     async delete(@ParamId() id: number) {
         return this.userService.delete(id);
